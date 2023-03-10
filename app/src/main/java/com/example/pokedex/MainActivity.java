b8c71d7016bb00dc6dd7cd02cd769f4ff09349c3
JehanPhlp <93180306+JehanPhlp@users.noreply.github.com>
2023-03-10 12:17:02 +0100

    ajout du bouton ajout dans la menu bar

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.pokedex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private List<String> listValeurs;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menubar, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +41,38 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.buttonCentre);
         ListView listview = (ListView) findViewById(R.id.listview);
 
+        this.listValeurs = new ArrayList<String>();
+        this.adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, this.listValeurs);
+
+        listview.setAdapter(this.adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = (String) parent.getItemAtPosition(position);
+
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, R.string.toast_button_clic, Toast.LENGTH_SHORT).show();
                 Log.v(TAG, "Bouton centre cliqué :Toast("+R.string.toast_button_clic+")");
-                listview.add()
+                String uniqueId = UUID.randomUUID().toString();
+                MainActivity.this.listValeurs.add(uniqueId);
+                MainActivity.this.adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.additem_menu:
+                
+        }
     }
 
 }
